eb463ae314ea899df599bec5ccc96fc71ee0551e
Jarkko Hulkko <jarkko.hulkko@student.jamk.fi>
2022-08-24 09:03:32 +0300

    W ja O toiminnot

--- src/project.c
+++ src/project.c
@@ -58,18 +58,18 @@ int calculate_total_points(struct student s)
     return sum;
 }
 
-void print_students(struct student *collection, unsigned int n)
+void print_students(struct student *collection, unsigned int n, FILE *stream)
 {
     struct student s;
     for (unsigned int i = 0; i < n; i++)
     {
         s = collection[i];
-        printf("%s %s %s ", s.id, s.firstname, s.lastname);
+        fprintf(stream, "%s %s %s ", s.id, s.firstname, s.lastname);
         for (int j = 0; j < 6; j++)
         {
-            printf("%d ", s.points[j]);
+            fprintf(stream, "%d ", s.points[j]);
         }
-        printf("%d\n", calculate_total_points(s));
+        fprintf(stream, "%d\n", calculate_total_points(s));
     }
 }
 
@@ -120,13 +120,14 @@ int compare(const void *a, const void *b)
     struct student *eka = (struct student *)a;
     struct student *toka = (struct student *)b;
 
-    return  (-1)*(calculate_total_points(*eka)-calculate_total_points(*toka));
+    return (-1) * (calculate_total_points(*eka) - calculate_total_points(*toka));
 }
 
 int main(void)
 {
     struct student *student_collection = NULL;
     unsigned int size = 0;
+    FILE *f = NULL;
     char buffer[1000];
     char command = 'X';
 
@@ -136,26 +137,27 @@ int main(void)
         command = buffer[0];
         int args;
         char id[100];
+        char firstname[100];
+        char lastname[100];
+        char filename[100];
+
         switch (command)
         {
         case 'A':
         {
-            char firstname[100];
-            char lastname[100];
             args = sscanf(buffer, "A %s %s %s", id, firstname, lastname);
 
-            if (student_already_in_collection(id, student_collection, size))
-            {
-                printf("Student %s is already in the database.\n", id);
-                break;
-            }
-            
             if (args != 3)
             {
                 printf("A should be followed by exactly 3 arguments.\n");
                 break;
             }
 
+            if (student_already_in_collection(id, student_collection, size))
+            {
+                printf("Student %s is already in the database.\n", id);
+                break;
+            }
             
             struct student new_student;
             int retval = init_student(&new_student, id, firstname, lastname);
@@ -208,10 +210,79 @@ int main(void)
         case 'L':
             // Sort students based on total points in ascending order
             qsort(student_collection, size, sizeof(struct student), compare);
-            print_students(student_collection, size);
+            print_students(student_collection, size, stdout);
             printf("SUCCESS\n");
             break;
+        case 'W':
+        {
+            args = sscanf(buffer, "W %s", filename);
+
+            if (args != 1)
+            {
+                printf("U should be followed by exactly 1 argument.\n");
+                break;
+            }
+
+            f = fopen(filename, "w");
+
+            if (!f)
+            {
+                printf("Cannot open file %s.\n", filename);
+                break;
+            }
+
+            qsort(student_collection, size, sizeof(struct student), compare);
+            print_students(student_collection, size, f);
+            printf("SUCCESS\n");
+            fclose(f);
+            break;
+        }
+        case 'O':
+        {
+            args = sscanf(buffer, "O %s", filename);
+
+            if (args != 1)
+            {
+                printf("O should be followed by exactly 1 argument.\n");
+                break;
+            }
+
+            f = fopen(filename, "r");
+
+            if (!f)
+            {
+                printf("Cannot open file %s for reading.\n", filename);
+                break;
+            }
+
+            // Delete old collection
+            delete_collection(student_collection, size);
+            student_collection = NULL;
+            size = 0;
+
+            int p1, p2, p3, p4, p5, p6;
+            
+            while (fgets(buffer, 1000, f) != NULL)
+            {
+                args = sscanf(buffer, "%s %s %s %d %d %d %d %d %d", id, firstname, lastname, &p1, &p2, &p3, &p4, &p5, &p6);
+                struct student new_student;
+                init_student(&new_student, id, firstname, lastname);
+                new_student.points[0] = p1;
+                new_student.points[1] = p2;
+                new_student.points[2] = p3;
+                new_student.points[3] = p4;
+                new_student.points[4] = p5;
+                new_student.points[5] = p6;
+                student_collection = add_student_to_collection(student_collection, size, new_student);
+                size++;
+            }
+
+            printf("SUCCESS\n");
+            fclose(f);
+            break;
+        }
         case 'Q':
+            printf("SUCCESS\n");
             break;
         default:
             printf("Invalid command %c\n", command);
